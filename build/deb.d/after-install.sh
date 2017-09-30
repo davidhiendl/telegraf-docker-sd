8d6acaef23f52f1e5b979065ffb606d0d28352c9
David Hiendl <david.hiendl@dhswt.de>
2017-09-30 11:46:31 +0200

    fixed debian post-install permissions and service

--- build/deb.d/after-install.sh
+++ build/deb.d/after-install.sh
@@ -4,11 +4,17 @@
 mkdir -p /etc/telegraf/telegraf.d
 mkdir -p /etc/telegraf/sd-tpl.d
 
+# write access to telegraf configuration folder is required
+# - pre-existing configuration files may be read-only as well (no -R needed)
+# - own configuration may be read only
+chown telegraf:telegraf /etc/telegraf/telegraf.d
+
 # ensure telegraf has access to docker socket
 usermod -a -G docker telegraf
 
 # reload init script
 systemctl daemon-reload
+systemctl enable telegraf-docker-sd.service
 
 # start daemon
 systemctl start telegraf-docker-sd.service
