3358d142497a1b737a989b664120c772d0fc6020
David Hiendl <david.hiendl@dhswt.de>
2018-03-18 16:24:58 +0100

    switched from Makefile to custom-made build bash script

---
+++ build.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+DOCKER_REPO=dhswt/telegraf-docker-sd
+BINARY_PATH=./dist
+BINARY_NAME=telegraf-docker-sd
+BINARY_TARGET="${BINARY_PATH}/${BINARY_NAME}"
+
+function build {
+   set-gopath
+   echo "building binary..."
+   echo "> target: ${BINARY_TARGET}"
+   go build -i -ldflags="-s -w" -o "${BINARY_TARGET}" ./main.go
+}
+
+function build-dev {
+   set-gopath
+   echo "building binary..."
+   echo "> target: ${BINARY_TARGET}"
+   go build -i -ldflags="-s -w" -o "${BINARY_TARGET}" ./main.go
+   upx "${BINARY_TARGET}"
+}
+
+function package-deb {
+    build
+    ./build/scripts.d/package-deb.sh
+}
+
+function set-gopath {
+    LOCAL_GO_PATH=$(realpath $PWD/../../../..)
+    GOPATH=$GOPATH:$LOCAL_GO_PATH
+}
+
+function glide {
+    set-gopath
+    glide "${@:2}"
+}
+
+function test-run-docker {
+	BACKENDS=docker \
+	CONFIG_DIR=./conf.test.d \
+	TEMPLATE_DIR=./sd-tpl.d \
+	GLOBAL_TAGS_ABC=some-value-a \
+	GLOBAL_TAGS_DEF=some-value-b \
+	GLOBAL_TAGS_GHI_JKL=some-value-c \
+	${BINARY_TARGET}
+}
+
+function image {
+    echo "Building telegraf-docker-sd image, this might take a long time..."; \
+	docker build --squash -t $DOCKER_REPO:dev .
+}
+
+case "$1" in
+    build)
+        build
+        ;;
+
+    build-dev)
+        build-dev
+        ;;
+
+    package-deb)
+        package-deb
+        ;;
+
+    glide)
+        glide
+        ;;
+
+    image)
+        image
+        ;;
+
+    test-run-docker)
+        test-run-docker
+        ;;
+
+    *)
+        echo $"Usage: $0 {build|build-dev|package-dev|glide|image|test-run-docker}"
+        exit 1
+esac
