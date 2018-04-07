ed9c2466e1e6dbbc532ea309a332dbead77e2ac7
David Hiendl <david.hiendl@dhswt.de>
2018-04-07 12:53:31 +0200

    improved reloader description

--- app/utils/telegraf_reloader.go
+++ app/utils/telegraf_reloader.go
@@ -52,10 +52,9 @@ func (tr *TelegrafReloader) Reload() {
 func (tr *TelegrafReloader) dispatchSignal() {
 
 	callback := func(path string, info os.FileInfo, err error) error {
-		// We just return in case of errors, as they are likely due to insufficient
-		// privileges. We shouldn't get any errors for accessing the information we
-		// are interested in. Run as root (sudo) and log the error, in case you want
-		// this information.
+		// We just return in case of errors, as they are likely due to insufficient privileges. We shouldn't get any
+		// errors for accessing the information we are interested in. Run as root (sudo) and log the error, in case you
+		// want this information.
 		if err != nil {
 			return nil
 		}
@@ -63,8 +62,7 @@ func (tr *TelegrafReloader) dispatchSignal() {
 		// We are only interested in files with a path looking like /proc/<pid>/status.
 		if strings.Count(path, "/") == 3 && strings.Contains(path, "/status") {
 
-			// Let's extract the middle part of the path with the <pid> and
-			// convert the <pid> into an integer. Log an error if it fails.
+			// extract the middle part of the path with the <pid> and convert it into an integer. Log on failure
 			pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
 			if err != nil {
 				logrus.Debugf("failed to extract pid from path: %v", path)
@@ -72,11 +70,10 @@ func (tr *TelegrafReloader) dispatchSignal() {
 			}
 
 			// The status file contains the name of the process in its first line.
-			// The line looks like "Name: theProcess".
-			// Log an error in case we cant read the file.
+			// The line looks like "Name: theProcess", log an error in case we cant read the file.
 			f, err := ioutil.ReadFile(path)
 			if err != nil {
-				log.Println(err)
+				logrus.Error("failed to read status file, %+v", err)
 				return nil
 			}
 
